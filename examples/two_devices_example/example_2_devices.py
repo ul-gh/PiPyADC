205aa4339aa3c9f9f90694d8b7f53627bd58a89d
Ulrich Lukas <u.lukas@kostal.com>
2025-07-01 15:13:32 +0200

    fix example

--- examples/two_devices_example/example_2_devices.py
+++ examples/two_devices_example/example_2_devices.py
@@ -33,7 +33,7 @@ print("\nPress CTRL-C to exit.\n")
 screen = TextScreen()
 
 
-def text_format_8_ch(digits: int, volts: float) -> str:
+def text_format_8_ch(digits: np.Array[int], volts: np.Array[float]) -> str:
     digits_str = ", ".join([f"{i: 8d}" for i in digits])
     volts_str = ", ".join([f"{i: 8.3f}" for i in volts])
     text = (
